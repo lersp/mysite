4902b240c09c2697545de7285e71878a46a7ceda
Lucas Pinto <lucas.lersp@gmail.com>
2026-03-10 00:37:50 -0300

    update index add js and json

---
+++ js/config.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./config.json')
+        .then(response => response.json())
+        .then(config => {
+            document.getElementById('nome').textContent = config.nome;
+            document.getElementById('cargo').textContent = config.cargo;
+
+            const lista = document.getElementById('lista-tech');
+            config.tecnologias.forEach(tech => {
+                let li = document.createElement('li');
+                li.textContent = tech;
+                lista.appendChild(li);
+            });
+
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo de configuração:', error);
+        });
+});
+
+//
