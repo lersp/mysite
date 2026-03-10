0fdf11a2c5f5c45f0378035211ac0668a340aeac
Lucas Pinto <lucas.lersp@gmail.com>
2026-03-10 15:18:40 -0300

    update index js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
             config.navBar.forEach(menu => {
                 let li = document.createElement('li');
                 li.innerHTML = `
-                    <div>teste: ${menu.menuName}</div>
+                    <div>${menu.menuName}</div>
                 `;
                 contNavBar.appendChild(li);
             });
