44103cc6ee8de7458d54127c8e61aaa849ec9924
Lucas Pinto <lucas.lersp@gmail.com>
2026-03-10 18:55:53 -0300

    update idx ren scriptjs del datajs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// menu from config.json
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('config.json')
+        .then(response => response.json())
+        .then(config => {
+            //titulo site (meta) ## funcionou ##
+            const projetoPrincipal = config.projetos[0];
+            document.getElementById('site-title').textContent = projetoPrincipal.titulo;
+
+            // menu ## nao funciona ##
+            const contNavBar = document.getElementById('navBar');
+            config.navBar.forEach(menu => {
+                contNavBar.innerHTML = `
+                    <a href="#${menu.menuName}">${menu.menuName}</a>
+                `;
+            });
+        })
+        .catch(err => {
+            console.error('Erro ao carregar o arquivo de configuração:', err);
+        });
+});
+
+// data from data.json
 document.addEventListener('DOMContentLoaded', () => {
     fetch('data.json')
         .then(response => response.json())
